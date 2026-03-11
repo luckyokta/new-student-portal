0812e67fdb5a63d9952e51575a4b073fe41ba3df
luckyokta <luckyvokta@gmail.com>
2026-03-11 13:59:27 +0700

    add models/Student

---
+++ models/Student.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.ObjectId
+
+const StudentSchema = new Schema({
+    id: ObjectId,
+    name: String,
+    dateOfBirth: Date,
+    studentNumber: {
+        type: Number,
+        unique: true
+    },
+    email: String,
+    department: String,
+    courses: [String],
+    userId: String
+}, { timestamps: true })
+
+const Student = mongoose.model('Student', StudentSchema)
+
+module.exports = Student 
